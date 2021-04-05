63bab65f43da5bd625bde5404eacd2dcf2773499
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-04-05 10:54:15 +0900

    class components mount and unmount

--- src/const/languages.js
+++ src/const/languages.js
@@ -1,1 +1,9 @@
-export const LANGUAGES = ["Javascript", "C++", "Ruby", "Java", "PHP", "Go"];
+const LANGUAGES = ["Javascript", "C++", "Ruby", "Java", "PHP", "Go"];
+
+export const getLanguages = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(LANGUAGES);
+    }, 1000);
+  });
+};
