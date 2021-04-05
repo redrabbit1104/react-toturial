b4bcd322a5d301dea7bdff658c24ed075cf2c67b
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-04-05 10:35:18 +0900

    mount and unmount for function components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
-import { LANGUAGES } from "./const/languages";
+import { getLanguages } from "./const/languages";
 
 function App() {
   // const [description, setDescription] = useState("クリック前の表示");
   const [tab, setTab] = useState("list");
-  const [langs, setLangs] = useState(LANGUAGES);
+  const [langs, setLangs] = useState([]);
+
+  useEffect(() => {
+    console.log("App.js:useEffect");
+    fetchLanguages();
+  }, []);
+
+  const fetchLanguages = async () => {
+    const languages = await getLanguages();
+    setLangs(languages);
+  };
 
   const addLang = (lang) => {
     // 追加
